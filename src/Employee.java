c22710bba48a437c2ab31f49f52af3ea44d4d64a
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-05-06 10:22:50 -0700

    Added the methods for the employee

--- src/Employee.java
+++ src/Employee.java
@@ -4,6 +4,7 @@ public class Employee {
     private String department;
     private double payRate;
     private double hoursWorked;
+    private Double lastPunchInTime = null;
 
     public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
         this.employeeId = employeeId;
@@ -23,6 +24,22 @@ public class Employee {
 
     public double getTotalPay() {
         return getRegularHours() * payRate + getOvertimeHours() * payRate * 1.5;
+    }
+
+    public void punchTimeCard(double time) {
+        if (lastPunchInTime == null) {
+            lastPunchInTime = time;
+            System.out.println(name + " punched in at " + time);
+        } else {
+            double worked = time - lastPunchInTime;
+            if (worked > 0) {
+                hoursWorked += worked;
+                System.out.println(name + " punched out at " + time + ". Worked " + worked + " hours.");
+            } else {
+                System.out.println("Invalid punch-out time.");
+            }
+            lastPunchInTime = null;
+        }
     }
 
 }
